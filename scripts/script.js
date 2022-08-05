53a6e6509ec98984c14ed49ea8fdc911da734bac
yktprojects <110669903+yktprojects@users.noreply.github.com>
2022-08-05 16:41:15 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
 
+//preloader code
+let loader=document.getElementById("preloader");
+            window.addEventListener('load',()=>loader.style.display='none')
+                
+            
+ //preloader code end            
