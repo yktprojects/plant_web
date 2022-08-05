68aa7b39d29906289c22deee71ea606c19a93a96
yktprojects <110669903+yktprojects@users.noreply.github.com>
2022-08-05 17:00:58 +0530

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-
-//preloader code
-let loader=document.getElementById("preloader");
-            window.addEventListener('load',()=>loader.style.display='none')
-                
-            
- //preloader code end            
